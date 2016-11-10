f3f3afc7b6f80a145e7a1e0e8c3579bd86498cb8
noitak <noitak@me.com>
2016-11-10 14:36:15 +0900

    add Pomodoro struct

--- pomodoroplayer.go
+++ pomodoroplayer.go
@@ -1,12 +1,32 @@
 package main
 
 import (
-	"os/exec"
 	"fmt"
+	"os/exec"
 	"time"
 )
 
 const playcmd = "/usr/bin/afplay" // mac only!!
+
+type Pomodoro struct {
+	worksongs []string
+	restsongs []string
+	worktime  time.Duration
+	resttime  time.Duration
+}
+
+func NewPomodoro(
+	worksongs []string,
+	restsongs []string,
+	worktime time.Duration,
+	resttime time.Duration) *Pomodoro {
+	return &Pomodoro{worksongs, restsongs, worktime, resttime}
+}
+
+func (p *Pomodoro) Start() {
+	working(p.worktime, p.worksongs)
+	working(p.resttime, p.restsongs)
+}
 
 func play(cmd *exec.Cmd, playend chan struct{}) {
 	cmd.Run()
@@ -22,24 +42,25 @@ func working(t time.Duration, worksongs []string) {
 
 			fmt.Printf("Start: %s\n", song)
 			go play(cmd, playend)
-			<- playend
+			<-playend
 		}
 	}()
 
 	time.Sleep(t)
 	cmd.Process.Kill()
-	
+
 }
 
 func main() {
-	worksongs := []string{
-		"./Japanese_School_Bell02-12.mp3",
-		"./Japanese_School_Bell02-12.mp3",
-	}
-	restsongs := []string{
-		"./gup01.mp4",
-	}
-
-	working(11 * time.Second, worksongs)
-	working(5 * time.Second, restsongs)
+	pomodoro := NewPomodoro(
+		[]string{
+			"./Japanese_School_Bell02-12.mp3",
+			"./Japanese_School_Bell02-12.mp3",
+		},
+		[]string{
+			"./gup01.mp4",
+		},
+		11*time.Second,
+		5*time.Second)
+	pomodoro.Start()
 }
